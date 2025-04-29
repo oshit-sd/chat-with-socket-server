ee91f4bf7bd49a335dcd9f2bd9211f6e87b84286
oshit.rul <oshit.dar.riseuplabs@gmail.com>
2025-04-29 11:57:18 +0600

    feat: refactor chat application structure and enhance socket connection handling

---
+++ src/components/elements/ChatMessages.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    ref="chatBox"
+    class="flex-1 overflow-y-auto px-4 py-6 space-y-4 bg-[#1e293b]"
+  >
+    <div
+      v-for="(msg, index) in messages"
+      :key="index"
+      class="flex"
+      :class="msg.user.id === currentUser.id ? 'justify-end' : 'justify-start'"
+    >
+      <div
+        class="max-w-[75%] px-4 py-2 rounded-xl text-sm shadow-md"
+        :class="
+          msg.user.id === currentUser.id
+            ? 'bg-indigo-800 text-white rounded-br-none'
+            : 'bg-slate-600 text-white rounded-bl-none'
+        "
+      >
+        <p>{{ msg.text }}</p>
+        <span class="block text-xs mt-1 text-right text-slate-300">
+          {{ msg.user.id === currentUser.id ? "You •" : msg.user.name }}
+          {{ formatTime(msg.time) }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    messages: Array,
+    currentUser: Object,
+  },
+  methods: {
+    formatTime(date) {
+      return new Date(date).toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    },
+  },
+};
+</script>
---
+++ src/components/elements/TypingIndicator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    v-if="Object.keys(typingUsers).length"
+    class="text-xs text-slate-400 px-4 pb-2"
+  >
+    <span
+      v-for="([userId, userName], idx) in Object.entries(typingUsers)"
+      :key="userId"
+    >
+      {{ userName }} is typing...<span
+        v-if="idx !== Object.entries(typingUsers).length - 1"
+        >,</span
+      >
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    typingUsers: Object,
+  },
+};
+</script>
